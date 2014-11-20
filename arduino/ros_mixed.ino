2a0caaac8f525df72799b7e40efd672b25c24af1
Nerdymech <weeeeeeerdopenguin@gmail.com>
2014-11-20 12:08:32 -0500

    fixed ros arduino code so it compiles

--- arduino/ros_mixed.ino
+++ arduino/ros_mixed.ino
@@ -23,59 +23,64 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
-#include "BMSerial.h"
-#include "RoboClaw.h"
+#include <BMSerial.h>
+#include <RoboClaw.h>
 
 //Roboclaw Address
-#define address 0x80
-//Setup Comms with Roboclaw for Uno-- connect pin 5 to S1, Roboclaw on mode 7 and option 3
+#define address 0x80;
+//Setup Comms with Roboclaw for Uno (0 = RX)-- connect TX (Arduino 1) to S1, Roboclaw on mode 7 and option 3
 RoboClaw roboclaw(5,6,10000);//10ms timeout
 //create node handle: creates pubs and subs and includes serial port comms
 ros::NodeHandle nh; 
-std_msgs::String driveCmd; //create string with name "driveCmd"
+geometry_msgs::Twist driveCmd; //create string with name "driveCmd"
 std_msgs::String driveFeedback; //driveFeedback will tell main computer what's going on
-std_msgs::String enc_vals; //create string w name "enc_vals" for odom feedback
-ros::Publisher enc_vals("enc_vals", &driveCmd); //created publisher w topic name "enc_vals"
-ros::Publisher lowlevelstate("lowlevelstate", &driveFeedback); 
-ros::Subscriber<std_msgs::Empty> sub(/cmd_dir, &drivecmd_Cb); //create subscriber w topic to subscribe to and callback function it uses
+//std_msgs::String enc_vals; //create string w name "enc_vals" for odom feedback
+// ros::Publisher enc_vals("enc_vals", &driveCmd); //created publisher w topic name "enc_vals"
+// ros::Publisher lowlevelstate("lowlevelstate", &driveFeedback); 
 
 //create callback function for subscriber
 //mixed mode requires drive and turn command
-void drivecmd_Cb(const std_msgs::String& driveCmd){
-	cmd = driveCmd.cmd;
-	if (cmd == "front"){
-		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd front";
-		roboclaw.ForwardMixed(address, 20); 
-		delay(2000);
-	}
-	else if (cmd == "left"){ //left + go forward
-		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd left";
-		roboclaw.LeftrightMixed(address, 54);
-		delay(2000);
-	}
-	else if (cmd == "right"){ //right + go forward
-		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd right";
-		roboclaw.LeftrightMixed(address, 74);
-		delay(2000);
-	}
-	else{
-		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd other";
-		roboclaw.ForwardMixed(address, 0);//stop if nobody 
-		delay(2000);
-	}
+void drivecmd_Cb(const geometry_msgs::Twist& driveCmd){
+	//cmd = driveCmd.cmd;
+
 }
+// 	if (cmd == "front"){
+// 		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd front";
+// 		roboclaw.ForwardMixed(address, 20); 
+// 		delay(2000);
+// 	}
+// 	else if (cmd == "left"){ //left + go forward
+// 		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd left";
+// 		roboclaw.LeftrightMixed(address, 54);
+// 		delay(2000);
+// 	}
+// 	else if (cmd == "right"){ //right + go forward
+// 		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd right";
+// 		roboclaw.LeftrightMixed(address, 74);
+// 		delay(2000);
+// 	}
+// 	else{
+// 		driveFeedback.cmd = "arduino.drivecmd_Cb: received /driveCmd other";
+// 		roboclaw.ForwardMixed(address, 0);//stop if nobody 
+// 		delay(2000);
+// 	}
+// }
+
+ros::Subscriber<geometry_msgs::Twist> sub("/cmd_dir", drivecmd_Cb); //create subscriber w topic to subscribe to and callback function it uses
+
 
 //in setup you also subscribe to topics you wish to listen to
 void setup() {
   roboclaw.begin(19200); //set roboclaw baud rate = 38400baud
   nh.initNode(); //init ros node handle
-  nh.advertise(enc_vals); //advertise topics being published
-  nh.subscribe(sub) //subscribe to topics to listen to
+ // nh.advertise(enc_vals); //advertise topics being published
+  nh.subscribe(sub); //subscribe to topics to listen to
 }
 
-void main() {
-  driveCmd.data = hello;
-  enc_vals.publish(&driveCmd);
+void loop() {
+  // driveCmd.data = hello;
+  // enc_vals.publish(&driveCmd);
+  // Serial.print(sub);
   nh.spinOnce();
   delay(1000); //wait 1000ms
   //check if drive command is forwards and distance to human is greater than 1 m
