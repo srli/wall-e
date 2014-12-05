402efa93f487740281ef389b2d6614d602c232f5
Nerdymech <weeeeeeerdopenguin@gmail.com>
2014-12-05 16:55:52 -0500

    added test code for driving with var speed servo

---
+++ arduino/WALL-E/varspeedservo_drivetest_statemachine/varspeedservo_drivetest_statemachine.ino
@@ -0,0 +1,101 @@
+/* switch statement with serial input
+ Open serial monitor and send i,k,j,l to move the robot.
+ Can't use Servo.h and VarSpeedServo.h at the same time.
+ */
+#include <VarSpeedServo.h>
+
+//VarSpeedServo rEye;
+//VarSpeedServo lEye;
+//VarSpeedServo huh;
+VarSpeedServo drive;
+VarSpeedServo turn;
+
+int pos = 0;    // variable to store the servo position 
+int ultrasonicsensorPin = A0;
+int sensorValue = 0;
+
+void setup(){
+  Serial.begin(9600); //setup serial comms
+  drive.attach(5);  // Fwd/back motion controlled by pin 5 to S1 
+  turn.attach(6);  // turning motion controlled by pin 6 to S2 
+  drive.write(90); //initialize drive
+  turn.write(90); //init turn
+  //  rEye.attach(2);
+  //  rEye.write(105,40,false); //set init pos at slow speed, running in background
+  //  lEye.attach(3);
+  //  lEye.write(86,40,false); //set init pos at slow speed, running in background.
+  //  huh.attach(4);
+  //  huh.write(90,40,true);
+  //  huh.write(120,15,true); //set init pos at slow speed, wait until done.
+  //  delay(1000);
+  //  huh.write(68,15,true);
+}
+
+
+void loop(){
+  sensorValue = analogRead(ultrasonicsensorPin);
+  //Serial.println(sensorValue);
+
+  if (Serial.available() > 0 && sensorValue >70){
+    int inByte = Serial.read();
+    //state machine that responds to char recieved
+    //single quotes tell controller to get ASCII value
+    //eg. 'a'=97
+    switch(inByte){
+      //    case 'h':
+      //      Serial.println("happy");
+      //      rEye.write(105,40,false);
+      //      lEye.write(69,40,false);
+      //      huh.write(90,35,true);
+      //      break;
+      //    case 'm':
+      //      Serial.println("sad");
+      //      rEye.write(92,15,false);
+      //      lEye.write(86,15,false);
+      //      huh.write(90,10,true);
+      //      break;
+      //    case 'n':
+      //      Serial.println("whoa");
+      //      rEye.write(130,25,false);
+      //      lEye.write(53,25,true);
+      //      break;
+    case 'i':
+      Serial.println("forwards");
+      drive.write(58);
+      turn.write(90);
+      break;
+    case 'j':
+      Serial.println("left");
+      turn.write(70);
+      break;
+    case 'l':
+      Serial.println("right");
+      turn.write(115);
+      break;
+    case 'k':
+      Serial.print("stop");
+      turn.write(90);
+      drive.write(90);
+      break;
+    default:
+      Serial.println("waiting");
+      turn.write(90);
+      drive.write(90);
+//      rEye.write(105,45,false);
+//
+//      lEye.write(69,45,false);
+//      huh.write(54,25,true); 
+
+    }
+  }
+  else
+  {
+    turn.writeMicroseconds(1500);
+    drive.writeMicroseconds(1500);
+  }
+
+}
+
+
+
+
