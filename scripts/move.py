040dfd41134de90355ddebb8150d71a4cb1fbc98
srli <sophia.li@students.olin.edu>
2014-12-17 09:38:00 -0500

    commented code for Adelas happiness and enjoyment

--- scripts/move.py
+++ scripts/move.py
@@ -8,13 +8,13 @@ from walle.msg import *
 
 def callback(data):
 	global message, prev_pos
-	xposition = int(data.positionx)
+	xposition = int(data.positionx) #we only care about the X position of the hand
 	print xposition
-	if xposition > 20:
+	if xposition > 50: #if x to left of center, we turn left to match
 		message = 1
-	if xposition < -20:
-		message = 3
-	else:
+	if xposition < -50: #if x to right of center, we turn right to match
+		message = 3 
+	else: #if x is += 50 of the center, we don't do anything 
 		message = 5	
 	if prev_pos == xposition:
 		message = 5
@@ -23,7 +23,7 @@ def callback(data):
 
 def publisher():
     rospy.init_node("centerfinger", anonymous = True)
-    rospy.Subscriber("/point_location", pointerpos, callback)
+    rospy.Subscriber("/point_location", pointerpos, callback) #subscribes to XYZ node from detectfinger
     pub = rospy.Publisher('/move', Char)
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
@@ -32,6 +32,7 @@ def publisher():
  		r.sleep()
        
 if __name__ == "__main__":
+	print "move.py initialized!"
 	message = 5
 	prev_pos = 0
    	publisher()
