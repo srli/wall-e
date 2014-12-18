b4bb785043649f9ca0dd6ca8563c6a2ab6c960a7
srli <sophia.li@students.olin.edu>
2014-12-18 12:28:14 -0500

    sound works!

---
+++ scripts/emotion_dummy.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+import roslib; roslib.load_manifest('walle')
+import rospy
+from std_msgs.msg import String
+from std_msgs.msg import Char
+from walle.msg import *
+import random
+from time import sleep
+
+def interaction_callback(data):
+	global message
+	wave = data.wave 
+	hello = data.hello
+	goodbye = data.goodbye
+	#our message fields are all booleans so we can do easy if statements
+	if wave:
+		print "waved!"
+		message = random.randint(8, 11) #sets messages as desired as reponses to detected gestures
+	if hello:
+		print "hello!"
+		message = 13
+	if goodbye:
+		print "goodbye"
+		message = 14
+	if not goodbye and not hello and not wave:
+		print "none work"
+		message = 0
+	print message
+
+
+def comms_callback(data):
+	global message
+	if "close" in data:
+		message = 6
+
+
+def publisher():
+	global message
+	rospy.init_node("dummy_interaction_node", anonymous = True)
+	# rospy.Subscriber("/detected_gestures", gestures, interaction_callback) #subscribing to detected gestures from detectfinger
+	# rospy.Subscriber("/drive_comms", String, comms_callback) #listens to feedback stream from the Arduino controlling the drive train
+	pub = rospy.Publisher('/emotion', Char) #publishes to emotion node, which the Arduino controlling WALL-E's eyes and ears listen to
+	r = rospy.Rate(2000)
+	while not rospy.is_shutdown():
+		msg = random.randint(8,11)
+		print msg
+		pub.publish(msg)
+		sleep(2)
+       
+if __name__ == "__main__":
+	print "emotions.py"
+	message = 0
+   	publisher()
